5b9466ebe3cf9b644369cd24643c1d8d960f63ad
rubens27-svg <rubens27@gmail.com>
2026-03-13 20:19:05 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
 const APP_CONFIG = {
     SUPABASE_URL : 'https://ywooulafmxwucedysuwa.supabase.co',
     SUPABASE_KEY : 'sb_publishable_9GaW4k4aE5yUjTSTVBYF3g_NZy4Fy7Y',   // ← substitua pela chave anon do painel Supabase
-    VERSION      : '2.0.1'
+    VERSION      : '2.0.0'
 };
